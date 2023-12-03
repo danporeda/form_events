ae13ecf5d10b8440a01749199f4b44ee498312f8
Dan <danporeda@yahoo.com>
2023-12-03 18:02:29 -0500

    eventListeners for all three forms

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const form = document.querySelector('#signup-form');
 
-const creditCard = document.querySelector('#cc');
+const creditCardInput = document.querySelector('#cc');
 const termsCheckbox = document.querySelector('#terms');
 const veggieSelect = document.querySelector('#veggie');
 
-form.addEventListener('submit', function(e){
-    alert('Submitted the form');
-    console.log(e);
-    e.preventDefault();
+const formData = {};
+creditCardInput.addEventListener('input', (e) => {
+    console.log("credit card changed", e);
+    formData['cc'] = e.target.value;
 })
+
+veggieSelect.addEventListener('input', (e) => {
+    console.log("veggie changed", e);
+    formData['veggie'] = e.target.value;
+})
+
+termsCheckbox.addEventListener('input', (e) => {
+    console.log('checkbox', e);
+    formData['agreeToTerms'] = e.target.checked;
+})
+
+// form.addEventListener('submit', function(e){
+//     alert('Submitted the form');
+//     console.log('cc', creditCardInput.value);
+//     console.log('terms', termsCheckbox.checked);
+//     console.log('veggie', veggieSelect.value);
+//     e.preventDefault();
+// })
