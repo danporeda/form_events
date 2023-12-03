da3ea58866a1b4d3b65e2097a126eede1b308b80
Dan <danporeda@yahoo.com>
2023-12-03 18:24:09 -0500

    refactored input eventListeners with for loop

--- app.js
+++ app.js
@@ -5,20 +5,28 @@ const termsCheckbox = document.querySelector('#terms');
 const veggieSelect = document.querySelector('#veggie');
 
 const formData = {};
-creditCardInput.addEventListener('input', (e) => {
-    console.log("credit card changed", e);
-    formData['cc'] = e.target.value;
-})
+for (let input of [creditCardInput, termsCheckbox, veggieSelect]) {
+    input.addEventListener('input', ({target}) => {
+        const {name, type, value, checked} = target;
+        formData[name] = type === 'checkbox' ? checked : value;
+    });
+}
 
-veggieSelect.addEventListener('input', (e) => {
-    console.log("veggie changed", e);
-    formData['veggie'] = e.target.value;
-})
 
-termsCheckbox.addEventListener('input', (e) => {
-    console.log('checkbox', e);
-    formData['agreeToTerms'] = e.target.checked;
-})
+// creditCardInput.addEventListener('input', (e) => {
+//     console.log("credit card changed", e);
+//     formData['cc'] = e.target.value;
+// })
+
+// veggieSelect.addEventListener('input', (e) => {
+//     console.log("veggie changed", e);
+//     formData['veggie'] = e.target.value;
+// })
+
+// termsCheckbox.addEventListener('input', (e) => {
+//     console.log('checkbox', e);
+//     formData['agreeToTerms'] = e.target.checked;
+// })
 
 // form.addEventListener('submit', function(e){
 //     alert('Submitted the form');
