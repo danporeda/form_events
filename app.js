84a05d68cc9588c782770f3f64df1412ead54780
Dan <danporeda@yahoo.com>
2023-12-03 18:33:01 -0500

    cleaned up html

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const veggieSelect = document.querySelector('#veggie');
 
 const formData = {};
 for (let input of [creditCardInput, termsCheckbox, veggieSelect]) {
-    input.addEventListener('input', ({target}) => {
+    input.addEventListener('change', ({target}) => {
         const {name, type, value, checked} = target;
         formData[name] = type === 'checkbox' ? checked : value;
+        console.log(formData);
     });
 }
 
